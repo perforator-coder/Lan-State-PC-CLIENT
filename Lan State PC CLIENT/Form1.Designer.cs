14340f9da33d6ff871d1a44f27eb1fec1b48d1b9
perforator-coder <nicita.paschkovsky@mail.ru>
2026-04-28 10:31:17 +0300

    Добавьте файлы проекта.

---
+++ Lan State PC CLIENT/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Lan_State_PC_CLIENT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            menuStrip1 = new MenuStrip();
+            оПрограммеToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            IP_SERVER_BOX = new TextBox();
+            PORT_SERVER_BOX = new TextBox();
+            NICK_CLIENT_BOX = new TextBox();
+            SAVE_PAM = new Button();
+            notifyIcon1 = new NotifyIcon(components);
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { оПрограммеToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(268, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            оПрограммеToolStripMenuItem.Size = new Size(94, 20);
+            оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Имя клиента:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Порт Сервера:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(24, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 15);
+            label3.TabIndex = 3;
+            label3.Text = "IP Сервера:";
+            // 
+            // IP_SERVER_BOX
+            // 
+            IP_SERVER_BOX.Location = new Point(99, 60);
+            IP_SERVER_BOX.Name = "IP_SERVER_BOX";
+            IP_SERVER_BOX.Size = new Size(100, 23);
+            IP_SERVER_BOX.TabIndex = 4;
+            // 
+            // PORT_SERVER_BOX
+            // 
+            PORT_SERVER_BOX.Location = new Point(99, 91);
+            PORT_SERVER_BOX.Name = "PORT_SERVER_BOX";
+            PORT_SERVER_BOX.Size = new Size(100, 23);
+            PORT_SERVER_BOX.TabIndex = 5;
+            // 
+            // NICK_CLIENT_BOX
+            // 
+            NICK_CLIENT_BOX.Location = new Point(99, 121);
+            NICK_CLIENT_BOX.Name = "NICK_CLIENT_BOX";
+            NICK_CLIENT_BOX.Size = new Size(100, 23);
+            NICK_CLIENT_BOX.TabIndex = 6;
+            // 
+            // SAVE_PAM
+            // 
+            SAVE_PAM.Location = new Point(42, 157);
+            SAVE_PAM.Name = "SAVE_PAM";
+            SAVE_PAM.Size = new Size(179, 23);
+            SAVE_PAM.TabIndex = 7;
+            SAVE_PAM.Text = "Сохранить и запустить ";
+            SAVE_PAM.UseVisualStyleBackColor = true;
+            SAVE_PAM.Click += SAVE_PAM_Click;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = "Программа работает в трее.\r\nКлиент запущен...";
+            notifyIcon1.BalloonTipTitle = "Lan State PC CLIENT";
+            notifyIcon1.Text = "Lan State PC CLIENT";
+            notifyIcon1.Visible = true;
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(268, 192);
+            Controls.Add(SAVE_PAM);
+            Controls.Add(NICK_CLIENT_BOX);
+            Controls.Add(PORT_SERVER_BOX);
+            Controls.Add(IP_SERVER_BOX);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Lan State PC CLIENT";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox IP_SERVER_BOX;
+        private TextBox PORT_SERVER_BOX;
+        private TextBox NICK_CLIENT_BOX;
+        private Button SAVE_PAM;
+        private NotifyIcon notifyIcon1;
+    }
+}
---
+++ Lan State PC CLIENT/Form1.cs
@@ -0,0 +1,83 @@
+namespace Lan_State_PC_CLIENT
+{
+    public partial class Form1 : Form
+    {
+        private string IP_serv;
+        private int PORT_serv;
+        private string NICK_client;
+        public Form1()
+        {
+            InitializeComponent();
+            //скрываем форму
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Icon = this.Icon;
+            if (!File.Exists("IP_SERV.txt") || !File.Exists("PORT_serv.txt") || !File.Exists("NICK_client.txt"))
+            {
+                //вызываем форму для изменения данных
+                this.Show();
+                this.notifyIcon1.Visible = false;
+                this.ShowInTaskbar = true;
+
+            }
+            else
+            {
+                //Иначе мы передаем данные из файлов в переменые
+                if (!int.TryParse(File.ReadAllText("PORT_serv.txt"), out PORT_serv))
+                {
+                    //вызываем форму для изменения
+                    this.Show();
+                    this.notifyIcon1.Visible = false;
+                    this.ShowInTaskbar = true;
+                }
+                else
+                {
+                    IP_serv = File.ReadAllText("IP_SERV.txt");
+                    NICK_client = File.ReadAllText("NICK_client.txt");
+                    IP_SERVER_BOX.Text = IP_serv;
+                    PORT_SERVER_BOX.Text = PORT_serv.ToString();
+                    NICK_CLIENT_BOX.Text = NICK_client;
+                }
+            }
+            // проверяем данные если они коректны то создаем TCP соединения
+            this.FormClosed += new FormClosedEventHandler(SaveClientData);
+        }
+
+        private void SAVE_PAM_Click(object sender, EventArgs e)
+        {
+            // проверяем введенные данные
+            if (string.IsNullOrWhiteSpace(IP_SERVER_BOX.Text) || !int.TryParse(PORT_SERVER_BOX.Text, out PORT_serv) || string.IsNullOrWhiteSpace(NICK_CLIENT_BOX.Text))
+            {
+                MessageBox.Show("Ошибка: Данные не коректны", "CLIENT DATA ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            IP_serv = IP_SERVER_BOX.Text;
+            NICK_client = NICK_CLIENT_BOX.Text;
+            // скрываем в трей программу
+            this.Hide();
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.ShowBalloonTip(10);
+          
+           
+        }
+        private void SaveClientData(object sender, EventArgs e)
+        {
+            // Записываем данные из txt
+            File.WriteAllText("IP_SERV.txt", IP_serv);
+            File.WriteAllText("PORT_serv.txt", PORT_serv.ToString());
+            File.WriteAllText("NICK_client.txt", NICK_client);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // при нажатии на приложение в трее открывается меню
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.notifyIcon1.Visible = false;
+            this.ShowInTaskbar = true;
+            // и останавливается клиенская часть
+        }
+    }
+}
